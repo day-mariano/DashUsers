83eeac867162f6c32d04b12503d7ff3070979d11
Dayana Mariano <dayanaraquelle@gmail.com>
2024-06-03 11:31:24 -0300

    feat: fetch dashboard

--- src/Pages/Dashboard/index.tsx
+++ src/Pages/Dashboard/index.tsx
@@ -1,10 +1,36 @@
 import { Header } from "../../Components/Header"
+import { useEffect, useState } from "react";
+
+interface Usuario {
+  name: string;
+  email: string;
+  password: string;
+  password_confirmation: string;
+  persistante: boolean;
+}
 
 const Dashboard = () => {
+  const [usuarios, setUsuarios] = useState<Usuario[]>([])
+  
+  useEffect(() => {
+    fetch("https://teste.reobote.tec.br/api/dashboard")
+      .then((res) => res.json())
+      .then((res) => {
+        console.log(res)
+        setUsuarios(res);
+      });
+      console.log(usuarios)
+  }, []);
+
   return (
   <>
     <Header/>
     <p>lista de usuários</p>
+    <ul>
+      {usuarios.map((usuario) => (
+        <li>{usuario.name}</li>
+      ))}
+    </ul>
   </>
   )
 }
